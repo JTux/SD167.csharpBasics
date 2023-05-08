4060801b495fca9c3a778835e77f673355a9301c
Joshua Tucker <jtucker@elevenfifty.org>
2023-05-08 14:26:28 -0400

    Created first C# Project

---
+++ 00_HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// console.log("Hello, World!");
+
+// C# is a compiled language
+// JavaScript is interpreted
+
+// Before running C#, we must build and compile it
+// dotnet build
+// dotnet run --project 00_HelloWorld
+// dotnet watch run --project 00_HelloWorld
+
+// dotnet watch provides a hot reload
+// This runs the application after changes are saved
+
+// ctrl+c to stop dotnet watch
+
+Console.WriteLine("My name is Joshua!");
