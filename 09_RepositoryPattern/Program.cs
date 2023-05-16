7d283c8e21532c969795564212039e4cf6014e9d
Joshua Tucker <jtucker@elevenfifty.org>
2023-05-16 15:34:47 -0400

    Update and Delete functionality

--- 09_RepositoryPattern/Program.cs
+++ 09_RepositoryPattern/Program.cs
@@ -2,6 +2,7 @@
 
 StreamingContent content = new StreamingContent();
 content.Title = "The SpongeBob Movie";
+content.Description = "Shelby's favorite movie.";
 
 Console.WriteLine(content.IsFamilyFriendly);
 Console.WriteLine(content.MaturityRating);
@@ -11,6 +12,44 @@ Console.WriteLine(content.IsFamilyFriendly);
 
 StreamingContentRepository repository = new StreamingContentRepository();
 
+List<StreamingContent> listBeforeAdd = repository.GetDirectory(); // <----
+
 bool wasAdded = repository.AddContentToDirectory(content);
 Console.WriteLine(wasAdded ? "The content was added" : "The content was not added");
 
+List<StreamingContent> listAfterAdd = repository.GetDirectory(); // <----
+
+Console.WriteLine(listBeforeAdd.Count);
+Console.WriteLine(listAfterAdd.Count);
+
+StreamingContent toystory = new StreamingContent("Toy Story", "Bromance", "A story about toys", 8.9, MaturityRating.G);
+var rubber = new StreamingContent("Rubber", "Action/Thriller", "It's a tire", 7.5, MaturityRating.R);
+repository.AddContentToDirectory(toystory);
+repository.AddContentToDirectory(rubber);
+
+List<string> titles = repository.GetTitles();
+foreach (var title in titles)
+{
+    Console.WriteLine(title);
+}
+
+StreamingContent? foundContent = repository.GetContentByTitle("the spongebob movie");
+// ?. only accesses the next part if not null
+// (foundContent?.Title) will only access Title if foundContent is not null
+Console.WriteLine(foundContent?.Title);
+Console.WriteLine(foundContent?.Genre);
+Console.WriteLine(foundContent?.Description);
+Console.WriteLine(foundContent?.IsFamilyFriendly);
+
+StreamingContent toyStory2 = new StreamingContent("Toy Story 2", "Bromance Part 2", "A story about toys", 7.5, MaturityRating.G);
+repository.UpdateExistingContent("Toy Story", toyStory2);
+
+foreach (var title in repository.GetTitles())
+{
+    Console.WriteLine(title);
+}
+
+// bool wasDeleted = repository.DeleteExistingContent(foundContent);
+bool wasDeleted = repository.DeleteContentByTitle("toy story");
+
+Console.WriteLine(wasDeleted ? "The content was deleted" : "The content was not deleted");
--- 09_RepositoryPattern/data/StreamingContentRepository.cs
+++ 09_RepositoryPattern/data/StreamingContentRepository.cs
@@ -58,10 +58,78 @@ public class StreamingContentRepository
         return new List<StreamingContent>(_contentDirectory);
     }
 
-    // Update
+    public StreamingContent? GetContentByTitle(string title)
+    {
+        // Loop through each streaming content instance in the directory
+        foreach (StreamingContent sc in _contentDirectory)
+        {
+            // Check that the title of sc (iterator) equals the title parameter
+            if (sc.Title?.ToLower() == title.ToLower())
+            {
+                // If the titles match, return sc
+                return sc;
+            }
+        }
 
+        // If we've looped through the entire directory and not found the title
+        return null;
+    }
+
+    // Update
+    public bool UpdateExistingContent(string originalTitle, StreamingContent newContent)
+    {
+        // Get the old content and verify it exists
+        StreamingContent? oldContent = GetContentByTitle(originalTitle);
+
+        if (oldContent == null) 
+        {
+            // If oldContent was not found, we return false
+            return false;
+        }
+
+        // oldContent was found and is not null
+
+        // Only need option 1 or option 2, not both:
+
+        // 1) Replace values for all properties
+        // The reference never changes
+        oldContent.Title = newContent.Title;
+        oldContent.Genre = newContent.Genre;
+        oldContent.Description = newContent.Description;
+        oldContent.MaturityRating = newContent.MaturityRating;
+        oldContent.UserScore = newContent.UserScore;
+        
+        // 2) Replace the reference held in the List
+        // Swap one stored reference for another
+        int index = _contentDirectory.IndexOf(oldContent);
+        _contentDirectory[index] = newContent;
+
+        return true;
+    }
 
     // Delete
+    // Remove existing content from our directory
+    public bool DeleteExistingContent(StreamingContent existingContent)
+    {
+        if (existingContent.UserScore > 9)
+        {
+            return false;
+        }
 
+        bool deleteResult = _contentDirectory.Remove(existingContent);
+        return deleteResult;
+    }
+
+    public bool DeleteContentByTitle(string title)
+    {
+        StreamingContent? targetContent = GetContentByTitle(title);
+        if (targetContent == null)
+        {
+            return false;
+        }
+
+        bool deleteResult = DeleteExistingContent(targetContent);
+        return deleteResult;
+    }
 
 }
